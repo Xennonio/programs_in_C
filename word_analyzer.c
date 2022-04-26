a147b4b85072faca3620ba79be4ec3e608d383a7
Xonhonho <102549312+Xonhonho@users.noreply.github.com>
2022-04-26 16:47:52 -0300

    Update word_analyzer.c

--- word_analyzer.c
+++ word_analyzer.c
@@ -1,30 +1,15 @@
-//program that calculates the number of vowels and consonants in a word
 #include <stdio.h>
-#include <string.h> //library to use string manipulators functions (specifically strlen())
+#include <string.h>
 
-int main() {
-  char v[30]; //local variable declaration to store the sentence
-  int i; //declaration of local variable to use in for loop
-  int j = 0, m = 0; //declaration of local variables to count the number of vowels and spaces in the sentence
+int main(){
+  char v[30];
+  int j = 0, m = 0;
   printf("choose a sentence of no more than 30 letters to analyze: ");
-  scanf("%[^\n]c", v); //choice of sentence
-  //analyze if there is a vowel in each character of the string
-  for (i = 0; i <= strlen(v); i++) {
-    if (v[i] == 'a'
-     || v[i] == 'e'
-     || v[i] == 'i'
-     || v[i] == 'o'
-     || v[i] == 'u'
-     || v[i] == 'A'
-     || v[i] == 'E'
-     || v[i] == 'I'
-     || v[i] == 'O'
-     || v[i] == 'U')
-      j++; //add one for each vowel that appears
-    if (v[i] == ' ')
-      m++; //add one for each space that appears
-  }
-  int k = strlen(v) - j - m; /*declaration of the local variable for the number of consonants
-  (string length minus the number of vowels minus the number of spaces)*/
+  scanf("%[^\n]c", v);
+  for (int i=0; i<=strlen(v); i++){
+    if (v[i]=='a'||v[i]=='e'||v[i]=='i'||v[i]=='o'||v[i]=='u'||v[i]=='A'||v[i]=='E'||v[i]=='I'||v[i]=='O'||v[i]=='U') j++;
+    if (v[i] == ' ') m++;
+	}
+  int k=strlen(v)-j-m;
   printf("the number of vowels is: %i\nthe number of consonants is: %i", j, k);
 }
